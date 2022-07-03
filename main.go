ea2e3a3e6c264eacab06ee3325613ee64c539805
Budianto <budiantojang@gmail.com>
2022-07-03 21:37:01 +0700

    created v1/kasus-seluruh-provinsi [get] endpoint

--- main.go
+++ main.go
@@ -45,10 +45,28 @@ func PemeriksaanDanVaksinasi(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(result))
 }
 
+func KasusSeluruhProvinsi(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	w.Header().Set("Content-Type", "application/json")
+	resp, err := http.Get("https://data.covid19.go.id/public/api/prov.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	result := string(body)
+	w.Write([]byte(result))
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("v1/update-harian-indonesia", UpdateHarianIndonesia)
 	http.HandleFunc("v1/pemeriksaan-dan-vaksinasi", PemeriksaanDanVaksinasi)
+	http.HandleFunc("v1/kasus-seluruh-provinsi", KasusSeluruhProvinsi)
 
 	log.Print("listening on :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
